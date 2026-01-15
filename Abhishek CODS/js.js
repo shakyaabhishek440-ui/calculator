e44cd7ed28edee096e5be449a1165c3d7309aa6f
shakyaabhishek440-ui <shakyaabhishek440@gmail.com>
2026-01-16 02:13:04 +0530

    Add files via upload

---
+++ Abhishek CODS/js.js
@@ -0,0 +1,36 @@
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    let value = e.target.innerHTML;
+
+    if (value === "=") {
+      try {
+        string = eval(string);
+        input.value = string;
+      } catch {
+        input.value = "Error";
+        string = "";
+      }
+    }
+
+    else if (value === "AC") {
+      string = "";
+      input.value = "";
+    }
+
+    else if (value === "DEL") {
+      string = string.slice(0, -1);
+      input.value = string;
+    }
+
+    else {
+      string += value;
+      input.value = string;
+      
+    }
+  });
+});
